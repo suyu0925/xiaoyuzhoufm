0450a8526f78894c97b8153f9d18f8bfcc92b76a
suyu <suyu0925@gmail.com>
2024-11-14 15:20:53 +0800

    optmize prompts

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import re
 import os
+import re
 
 import requests
 import whisper
 from bs4 import BeautifulSoup
 from openai import OpenAI
+from tqdm import tqdm
 
 from config import config
 
@@ -61,91 +62,70 @@ def sst(audio_file: str):
     model = whisper.load_model("turbo")
     result = model.transcribe(audio_file, language="Mandarin")
     text = '\n'.join([x['text'] for x in result['segments']])
-    output = change_file_extension(audio_file, ".txt")
-    open(output, "w", encoding='utf-8').write(text)
-    return audio_file
+    text_file = change_file_extension(audio_file, ".txt")
+    open(text_file, "w", encoding='utf-8').write(text)
+    return text_file
 
 
-def proofhead(text_file: str):
+def _proofread(text: str):
     client = OpenAI(
         api_key=config['api_key'],
         base_url=config['base_url'],
     )
     messages = [
-        {"role": "system", "content": "你是一个文字处理专家，擅长校正播客节目语音识别后的文章和要点总结。"},
-    ]
-    messages.append({
-        "role": "user",
-        "content": f"""
-这是一档播客节目的语音识别结果，请将识别出的文字中的错别字修正，加上合适的标点符号，分成合适的段落，并去掉多余的语气词和重复口癖。
-文章内容如下：
-{open(text_file, "r", encoding='utf-8').read()}
-"""})
-    proofhead_text = ""
-    index = 1
-    finished = False
-    while index < 30 and not finished:
-        response = client.chat.completions.create(
-            model="deepseek-chat",
-            messages=messages,
-            temperature=1.3,
-            max_tokens=1024,
-            stream=False,
-        )
-        messages.append(response.choices[0].message)
-        index += 1
-        proofhead_text += response.choices[0].message.content
-        finished = response.choices[0].finish_reason == "stop"
-        if not finished:
-            messages.append({
-                "role": "user",
-                "content": "继续",
-            })
-        print(index, response)
-    proofhead_file = add_file_surfix(text_file, "proofhead")
-    open(proofhead_file, "w", encoding='utf-8').write(proofhead_text)
-    return proofhead_file
+        {
+            "role": "user",
+            "content": f"""
+你是一位精通中文的文稿编辑，擅长对已经记录下来的文字稿进行润色、修改、校对和整理，以确保文稿的准确性、流畅性和专业性。我希望你能帮我将速记员记录下的文字稿调整语法、拼写、标点符号，优化句子结构，甚至对内容进行适当的增删和调整，以使文稿更加清晰、连贯和符合预期的风格要求。
 
+规则：
+- 结合上下文修正一些速记员的拼音错误，例如：“它们” -> “他们”。
+- 保留特定的英文术语或名字，并在其前后加上空格，例如："中 UN 文"。
+- 去除多余的口语外表达，例如：“嗯”，“然后”，“OK”。
+- 分成两次编辑，并且打印每一次结果：
+1. 只调整语法、拼音错误、标点符号，优化句子结构，但不改变原文的意思，不增加新的内容
+2. 根据第一次调整的结果再次编辑，遵守原意的前提下让内容更通俗易懂，去除没必要的换行，符合书面语表达习惯
 
-def summary(text_file: str):
-    client = OpenAI(
-        api_key=config['api_key'],
-        base_url=config['base_url'],
+以下是需要润色的文稿，请按照上面的规则打印两次编辑结果：
+{text}
+"""
+        }]
+    response = client.chat.completions.create(
+        model="deepseek-chat",
+        messages=messages,
+        temperature=1.1,
+        stream=False,
     )
-    messages = [
-        {"role": "system", "content": "你是一个文字处理专家，擅长校正语音识别出的文章和要点总结。"},
-    ]
-    messages.append({
-        "role": "user",
-        "content": f"""
-总结下面文章中的要点。
-文章内容如下：
-{open(text_file, "r", encoding='utf-8').read()}
-"""})
-    summary_text = ""
-    index = 1
-    finished = False
-    while index < 30 and not finished:
-        response = client.chat.completions.create(
-            model="deepseek-chat",
-            messages=messages,
-            temperature=1.3,
-            max_tokens=1024,
-            stream=False,
-        )
-        messages.append(response.choices[0].message)
-        index += 1
-        summary_text += response.choices[0].message.content
-        finished = response.choices[0].finish_reason == "stop"
-        if not finished:
-            messages.append({
-                "role": "user",
-                "content": "继续",
-            })
-        print(index, response)
-    summary_file = add_file_surfix(text_file, "summary")
-    open(summary_file, "w", encoding='utf-8').write(summary_text)
-    return summary_file
+    output = response.choices[0].message.content
+    proofread_text = output.split("### 第二次编辑结果")[1]
+    return proofread_text
+
+
+def split_text(text: str):
+    lines = text.split('\n')
+    chunk = ''
+    for line in lines:
+        chunk += line + '\n'
+        if len(chunk) > 1000:
+            yield chunk
+            chunk = ''
+    if chunk:
+        yield chunk
+
+
+def proofread(text_file: str):
+    input_text = open(text_file, "r", encoding='utf-8').read()
+
+    proofread_text = ""
+    pbar = tqdm(total=len(input_text))
+    for chunk in split_text(input_text):
+        proofread_text += _proofread(chunk)
+        pbar.update(len(chunk))
+    pbar.close()
+
+    proofread_file = add_file_surfix(text_file, "proofread")
+    open(proofread_file, "w", encoding='utf-8').write(proofread_text)
+    return proofread_file
 
 
 if "__main__" == __name__:
@@ -153,5 +133,6 @@ if "__main__" == __name__:
     data = parse_url(url)
     audio_file = download_audio(data['title'], data['audio_url'])
     text_file = sst(audio_file)
-    proofhead_file = proofhead(text_file)
-    summary_file = summary(proofhead_file)
+    # text_file = 'workspace/No 21 厌学，或许不仅仅是厌学：深入探讨孩子的内心世界.txt'
+    proofread_file = proofread(text_file)
+    print(f"文稿润色完成，结果保存在 {proofread_file}")
